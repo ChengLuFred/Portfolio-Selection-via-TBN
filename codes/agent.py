eb85f12d9d115d180263ff2d21b5d1e097636f31
Cheng <cheng.lu.fred@gmail.com>
2020-10-23 15:42:27 -0400

    first agent demo: finish parent class PolicyGradient and add child class ReinforceBaselineAgent

--- codes/agent.py
+++ codes/agent.py
@@ -49,8 +49,92 @@ class PolicyGradient:
         """
         policy part: return the probability mass function(pmf) 
         """
+        # preference vector for three actions
+        h = []
+        for action in self.ACTIONS:
+            h.append(self.get_h(position, action))
+
+        # soft-max in action preference
+        t = np.exp(h - np.max(h))
+        pmf = t / np.sum(t)       
+
+        return pmf
 
     def choose_action(self, position, reward):
         """
         return the action according to the policy
         """
+                # recording the reward, state for update using
+        if reward is not None:
+            self.rewards.append(reward)
+        if position is not None:
+            self.states.append(position)
+
+        # choose the action accordingly
+        pmf = self.get_pi(position)
+        action = np.random.choice(3, p=pmf) - 1      
+        self.actions.append(action)
+
+        return action
+
+class ReinforceBaselineAgent(PolicyGradient):
+    def __init__(self, alpha, alpha_w, gamma, num_of_tilings = 2, max_size = 2048):
+        PolicyGradient.__init__(self, alpha, gamma, num_of_tilings, max_size)
+        self.alpha_w = alpha_w / num_of_tilings
+
+        # a new hash table for baseline
+        self.hash_table_baseline = IHT(max_size)
+
+        # new weights vector for baseline
+        self.weights_baseline = np.zeros(max_size)
+
+    def get_active_tiles_baseline(self, position):
+        """
+        get indices of active tiles for given state
+        """
+        active_tiles = tiles(self.hash_table_baseline, 
+                             self.num_of_tilings,
+                            [self.position_scale * position])
+        #print("position is ", self.position_scale * position)
+        return active_tiles # tiles index in each tilings
+
+    def episode_end(self):
+        """
+        clear all memory
+        """
+        gamma_pow = 1
+
+        # compute total return from the end to start
+        G = np.zeros(len(self.rewards))
+        G[-1] = self.rewards[-1]
+        for i in range(2, len(G) + 1):
+            G[-i] = self.gamma * G[-i + 1] + self.rewards[-i]        
+        
+        # learn theta
+        for i in range(len(G)):
+            pmf = self.get_pi(self.states[i])
+            active_tiles = self.get_active_tiles_baseline(self.states[i])
+            baseline = np.sum(self.weights_baseline[active_tiles])
+            delta = G[i] - baseline
+
+            # update baseline
+            update_baseline = self.alpha_w * delta
+            for active_tile in active_tiles:
+                self.weights_baseline[active_tile] += update_baseline
+
+            # update agent
+            for action in self.ACTIONS:
+                if action == self.actions[i]:
+                    grad_ln_pi = 1 - pmf[action + 1]
+                else:
+                    grad_ln_pi = -pmf[action + 1]
+                update = self.alpha * gamma_pow * baseline * grad_ln_pi
+                active_tile = self.get_active_tiles(self.states[i], action)
+                self.weights[active_tile] += update
+
+            gamma_pow *= self.gamma
+
+        # clear the record at the end waiting for another iteration
+        self.rewards = []
+        self.actions = []
+        self.states = [0]
